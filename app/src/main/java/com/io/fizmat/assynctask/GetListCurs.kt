1df871aa3e82541d4a5a83556d4fac14cd01384d
KurakIvan <kurak.van9@gmail.com>
2020-04-20 12:06:02 +0300

    end commit

--- app/src/main/java/com/io/fizmat/assynctask/GetListCurs.kt
+++ app/src/main/java/com/io/fizmat/assynctask/GetListCurs.kt
@@ -4,6 +4,7 @@ import android.os.AsyncTask
 import com.io.fizmat.xlsreader.XLSReaderBRSU
 import com.io.fizmat.xlsreader.model.Curs
 import java.io.BufferedReader
+import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.URL
 
@@ -11,18 +12,24 @@ import java.net.URL
 class GetListCurs : AsyncTask<Void,Void,List<Curs>>(){
 
     override fun doInBackground(vararg params: Void?): List<Curs>? {
-        val url = URL("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1-j9vzxQP6WOr4eQS74Iu60aHnqb5yyaF")
-        val inputStream = url.openStream()
-        val reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"), 8)
 
-        val xlsReaderBRSU = XLSReaderBRSU.newInstance(URL(reader.readLine()).openStream())
+        try {
+            val url =
+                URL("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1-j9vzxQP6WOr4eQS74Iu60aHnqb5yyaF")
+            var inputStream = url.openStream()
+            val reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"), 8)
 
-        reader.close()
-        val list = xlsReaderBRSU.readXLS()
-        if (list != null)
+            val str = reader.readLine()
+
+            reader.close()
+            inputStream = URL(str).openStream()
+            val xlsReaderBRSU = XLSReaderBRSU.newInstance(inputStream)
+
+            inputStream.close()
             return xlsReaderBRSU.readXLS()
-        else
-            return listOf()
+        } catch (e: KotlinNullPointerException) {
+            return arrayListOf()
+        }
     }
 
 }
